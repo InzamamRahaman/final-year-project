8b647ae95c9e142b83a9c6fedc0365710281241c
Inzamam <izzyrahaman@gmail.com>
2015-03-31 18:02:09 -0400

    :( Still not working

--- python-decoder/src/OrganizingList.py
+++ python-decoder/src/OrganizingList.py
@@ -10,17 +10,17 @@ class OrganizingList(object):
         self.elements = [-1] * max_list_size
         self.num_elements = 0
         self.max_size = max_list_size
-        
+
     def migrate_down(self, lo, hi):
         for idx in range(hi, lo - 1, -1):
             self.elements[idx] = self.elements[idx - 1]
-    
+
     def find(self, elem):
         for (idx, element) in enumerate(self.elements):
             if element == elem:
                 return idx
         return -1
-    
+
     def insert(self, elem):
         location = self.find(elem)
         if location == -1:
@@ -31,22 +31,22 @@ class OrganizingList(object):
         elif location == 0:
             return 1
         for idx in range(location, 0, -1):
-            temp = self.elements[idx]
             self.elements[idx] = self.elements[idx - 1]
-            self.elements[idx - 1] = temp
+        # for idx in range(location, 0, -1):
+        #     temp = self.elements[idx]
+        #     self.elements[idx] = self.elements[idx - 1]
+        #     self.elements[idx - 1] = temp
         self.elements[0] = elem
         return location + 1
-    
+
     def __getitem__(self, index):
         end_idx = index - 1
         temp = self.elements[end_idx]
-        for idx in range(end_idx, -1, -1):
-            temp_in_loop = self.elements[idx + 1]
-            self.elements[idx + 1] = self.elements[idx]
-            self.elements[idx] = temp_in_loop
+        for idx in range(end_idx, 0, -1):
+            self.elements[idx] = self.elements[idx - 1]
+        self.elements[0] = temp
+        # for idx in range(end_idx, -1, -1):
+        #     temp_in_loop = self.elements[idx]
+        #     self.elements[idx] = self.elements[idx - 1]
+        #     self.elements[idx - 1] = temp_in_loop
         return temp
-            
-            
-                
-                
-            
--- python-decoder/src/decoder.py
+++ python-decoder/src/decoder.py
@@ -21,20 +21,84 @@ my_list = OrganizingList()
 queue_of_output = create_queue_of_data(input_file_name)
 
 
-def read_list_index():
-    zero_count = 0
+
+
+
+def read_list_indexV2():
+    bits = ''
+    bit = (queue_of_output.get())
+    while bit == '0':
+        bits = bits + bit
+        bit = queue_of_output.get()
+    bits = bits + bit
+    bit = queue_of_output.get()
+    bits = bits + bit
+    print "Checking ", bits
+    index = bin_to_int(bits)
+    print "Trying to get index ", index
+    print "List elements: ", my_list.elements
+    list_content = my_list[index]
+    image_file.write(str(list_content) + '\n')
+
+def read_list_index(start_count = 0):
+    zero_count = start_count
     bit = int(queue_of_output.get())
     while bit == 0:
         zero_count += 1
         bit = int(queue_of_output.get())
-    zero_count += 1
+    if not zero_count:
+        zero_count += 1
     num = str(bit)
     while zero_count:
         zero_count -= 1
         num += str(queue_of_output.get())
-    image_file.write(str(my_list[bin_to_int(num)]) + '\n')
+    print "Checking ", num
+    index = bin_to_int(num)
+    print "Trying to get index ", index
+    print "List elements: ", my_list.elements
+    list_content = my_list[index]
+    image_file.write(str(list_content) + '\n')
+
+def write_secret_bits(bits):
+    secret = bits[0]
+    for bit in secret:
+        secret_file.write(bit + '\n')
 
 
+def handle_case_3():
+    # read until we have all other zeroes
+    count = 0
+    bit = queue_of_output.get()
+    while bit == '0':
+        count += 1
+        bit = queue_of_output.get()
+    number = bit
+    number = number + get_k_bits(queue_of_output, count - 1)
+    print "converting ", number
+    index = bin_to_int(number)
+    print "to --->", index
+    print "List elements ", my_list.elements
+    print "Retrieving"
+    list_cont = my_list[index]
+    image_file.write(str(list_cont) + '\n')
+
+def handle_case_5():
+    # read until we have all other zeroes
+    count = 0
+    bit = queue_of_output.get()
+    while bit == '0':
+        count += 1
+        bit = queue_of_output.get()
+    count += 1
+    number = bit
+    number = number + get_k_bits(queue_of_output, count - 1)
+    print "converting ", number
+    index = bin_to_int(number)
+    print "to --->", index
+    print "List elements ", my_list.elements
+    print "Retrieving"
+    list_cont = my_list[index]
+    image_file.write(str(list_cont) + '\n')
 
 
 def main():
@@ -42,24 +106,26 @@ def main():
         bits = get_k_bits(queue_of_output, num_bits_per_index + 1)
         scenario = compute_case(bits)
         print "read ", str(bits)
-        if StreamPop.scenario1 == scenario:
+        if bits == '00':
             index_bits = get_k_bits(queue_of_output, 8)
             index = bin_to_int(index_bits)
+            print my_list.elements
             my_list.insert(index)
+            print my_list.elements
+            print "Writing ", index
             image_file.write(str(index) + '\n')
-        elif StreamPop.scenario2 == scenario:
-            secret = bits[0 : num_bits_per_index]
-            for bit in secret:
-                secret_file.write(bit + '\n')
+        elif bits == '01':
+            write_secret_bits(bits)
             next_bit = queue_of_output.get()
             if int(next_bit):
                 image_file.write(str(my_list[1]) + '\n')
             else:
-                read_list_index()
-        elif StreamPop.scenario3 == scenario:
+                handle_case_3()
+        elif bits == '10':
+            handle_case_5()
+        elif bits == '11':
+            write_secret_bits(bits)
             image_file.write(str(my_list[1]) + '\n')
-        else:
-            read_list_index()
     print "Finished with decoding process"
 
 
